36c9c902545f5737d805f4f9a338d5b487e215ed
Jared Grove <jaredkgrove@gmail.com>
2019-04-24 02:17:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,4 +13,5 @@ create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta li span").text
-# percent funded: project.css("ul.project-stats li.first.funded strong").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
